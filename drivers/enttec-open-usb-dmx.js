3297dd0f18a2ec8951a397f3759e2d568a037594
finefin <info@finefin.com>
2020-09-23 08:34:41 +0200

    Update enttec-open-usb-dmx.js

--- drivers/enttec-open-usb-dmx.js
+++ drivers/enttec-open-usb-dmx.js
@@ -6,7 +6,7 @@ function EnttecOpenUsbDMX(device_id, options) {
 	var self = this
 	options = options || {}
 
-	this.universe = Buffer.alloc(513)
+	this.universe = new Buffer(513)
 	this.universe.fill(0)
 
 	self.interval = 512
