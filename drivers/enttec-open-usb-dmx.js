e091b0e66e733a392c0466f5fd51a3ae95debb6c
finefin <info@finefin.com>
2020-09-24 08:25:28 +0200

    Update enttec-open-usb-dmx.js

--- drivers/enttec-open-usb-dmx.js
+++ drivers/enttec-open-usb-dmx.js
@@ -50,14 +50,7 @@ EnttecOpenUsbDMX.prototype.send_universe = function() {
 		setTimeout(function() {
 			self.dev.set({brk: false}, function(err, r) {
 				setTimeout(function() {
-					if (self.readyToWrite) {
-						console.log("readyToWrite");
-            self.readyToWrite = false;
-            self.dev.write(Buffer.concat([Buffer([0]), self.universe.slice(1)]));
-            self.dev.drain(() => {
-              self.readyToWrite = true;
-            });
-          }
+					self.dev.write(Buffer.concat([Buffer([0]), self.universe.slice(1)]))
 				}, 1)
 			})
 		}, 1)
