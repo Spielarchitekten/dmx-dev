9c0e26926b9286c9cf7c88084592b34fbd656708
finefin <info@finefin.com>
2020-10-20 13:32:53 +0200

    changed channel indexing & buffer alloc

--- drivers/enttec-open-usb-dmx.js
+++ drivers/enttec-open-usb-dmx.js
@@ -21,22 +21,24 @@ function EnttecOpenUsbDMX(device_id, options) {
 	var self = this
 	options = options || {}
 
-	this.universe = new Buffer(512)
-	this.universe.fill(0)
-	self.interval = 46
 
-	this.dev = new SerialPort(device_id, {
-		'baudrate': 250000,
-		'databits': 8,
-		'stopbits': 2,
-		'parity': 'none'
-	}, function(err) {
+	this.universe = Buffer.alloc(513);
+  this.readyToWrite = true;
+  this.interval = 46;
+
+  this.dev = new SerialPort(deviceId, {
+    'baudRate': 250000,
+    'dataBits': 8,
+    'stopBits': 2,
+    'parity': 'none',
+  }, function(err) {
 		if(err) {
 			console.log(err)
 			return
 		}
 		self.start()
-	})
+  });
+
 }
 
 EnttecOpenUsbDMX.prototype.send_universe = function() {
@@ -50,8 +52,7 @@ EnttecOpenUsbDMX.prototype.send_universe = function() {
 		setTimeout(function() {
 			self.dev.set({brk: false, rts: true}, function(err, r) {
 				setTimeout(function() {
-					//self.dev.write(Buffer.concat([Buffer([0]), self.universe.slice(1)]))
-					self.dev.write(Buffer.concat([Buffer([0]), self.universe]))
+					self.dev.write(Buffer.concat([Buffer([0]), self.universe.slice(1)]));
 				}, 1)
 			})
 		}, 1)
@@ -76,12 +77,11 @@ EnttecOpenUsbDMX.prototype.update = function(u) {
 	for(var c in u) {
 		this.universe[c] = u[c]
 	}
-	// this.emit('update',u);
 }
 
 EnttecOpenUsbDMX.prototype.updateAll = function(v) {
 	console.log("\nEnttecOpenUsbDMX updateAll: ", v);
-	for(var i = 0; i < 512; i++) {
+	for(var i = 1; i <= 512; i++) {
 		this.universe[i] = v
 	}
 }
